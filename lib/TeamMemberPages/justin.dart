7275c0d1467b2a871304ff5cb0af5118402c9f01
Justin Chairez <jtchairez@gmail.com>
2020-11-01 09:57:51 -0800

    Minor changes :construction:
    More changes to come in the week hopefully!!!
    :lipstick: Tested with added an image from web
    :lipstick: Tested color background color palettes

--- lib/TeamMemberPages/justin.dart
+++ lib/TeamMemberPages/justin.dart
@@ -10,12 +10,20 @@ class _JustinPageState extends State<JustinPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Text(
-          "Justin's Page",
-          style: TextStyle(fontSize: 50),
-        ),
+      appBar: AppBar(
+        title: Text('Justin\'s Page'),
+        backgroundColor: Colors.red,
       ),
+      body: Column(children: [
+        Text(
+          'Welcome to my personalized widget! Hope this gets',
+          style: TextStyle(
+            color: Colors.white, fontSize: 30.0, fontWeight: FontWeight.bold,),
+        ),
+        Image(
+          image: NetworkImage('https://lampstudy.net/wp-content/uploads/2020/04/Is-Computer-science-hard.jpg'),
+        ),
+      ]),
     );
   }
 }
