357f8823a5b10cd73ab97c7f1f2d972dd91fd968
Andrew Yip <andrew.yip22@gmail.com>
2020-11-01 10:35:02 -1000

    added an image and changed the font 📸

--- lib/TeamMemberPages/andrew.dart
+++ lib/TeamMemberPages/andrew.dart
@@ -1,5 +1,15 @@
+import 'package:dashboard_reborn/pages/about_page.dart';
+import 'package:dashboard_reborn/pages/gradients_page.dart';
+import 'package:dashboard_reborn/pages/material_page.dart';
+import 'package:dashboard_reborn/utils/colors.dart';
+import 'package:dashboard_reborn/utils/text_styles.dart';
+import 'package:dashboard_reborn/utils/ui_helpers.dart';
+import 'package:dashboard_reborn/widgets/sexy_tile.dart';
+import 'package:dynamic_theme/dynamic_theme.dart';
+import 'package:eva_icons_flutter/eva_icons_flutter.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:gradient_widgets/gradient_widgets.dart';
 
 class AndrewPage extends StatefulWidget {
   @override
@@ -11,14 +21,163 @@ class _AndrewPageState extends State<AndrewPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: Center(
-          child: Text(
-            "Andrew Yip",
-            style: TextStyle(fontSize: 50),
-          ),
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            //gradient colors change according to set theme
+              colors: isThemeCurrentlyDark(context)
+                  ? [GradientColors.darkStart, GradientColors.darkEnd]
+                  : [GradientColors.lightStart, GradientColors.lightEnd],
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              tileMode: TileMode.clamp),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            // SECTION FOR NAVBAR
+            Padding(
+              padding: EdgeInsets.only(
+                left: 10.0,
+                top: 50.0,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  IconButton(
+                    icon: Icon(EvaIcons.arrowIosBack),
+                    tooltip: 'Go back',
+                    color: isThemeCurrentlyDark(context) ? MaterialColors.teal : MyColors.light,
+                    iconSize: 26.0,
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                  ),
+                  GradientText(
+                    "Dev Team's Cards",
+                    shaderRect: Rect.fromLTWH(0.0, 0.0, 50.0, 50.0),
+                    gradient: isThemeCurrentlyDark(context)
+                        ? Gradients.coldLinear
+                        : Gradients.haze, //gradient colors change according to set theme
+                    style: isThemeCurrentlyDark(context) ? TitleStylesDefault.white : TitleStylesDefault.black,
+                  ),
+                ],
+              ),
+            ),
+
+            // END NAVBAR SECTION
+
+            // START OF MAIN BODY
+            Padding(
+              padding: const EdgeInsets.only(
+                top: 10,
+                bottom: 10,
+              ),
+              child: Center(
+                child: Text(
+                  "Andrew's Page",
+                  style: TextStyle(fontSize: 50, fontFamily: 'Courier New', color: Colors.green),
+                ),
+              ),
+            ),
+            Expanded(
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Center(
+                          child: Image(image: AssetImage('assets/credits/urmil.png'))
+                      ),
+                    ),
+                    SexyTile(
+                      splashColor: MyColors.accent,
+                      child: Padding(
+                        padding: const EdgeInsets.all(25),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            RaisedButton(
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(18.0), side: BorderSide(color: Colors.blue[100])),
+                              child: Text(
+                                "Gradients Page",
+                                style: isThemeCurrentlyDark(context) ? BodyStylesDefault.white : BodyStylesDefault.black,
+                              ),
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  CupertinoPageRoute(
+                                    builder: (context) {
+                                      return MyGradientsPage();
+                                    },
+                                  ),
+                                );
+                              },
+                            ),
+                            RaisedButton(
+                              shape: RoundedRectangleBorder(borderRadius: new BorderRadius.circular(30.0)),
+                              child: Text(
+                                "Material Page",
+                                style: isThemeCurrentlyDark(context) ? BodyStylesDefault.white : BodyStylesDefault.black,
+                              ),
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  CupertinoPageRoute(
+                                    builder: (context) {
+                                      return MyMaterialPage();
+                                    },
+                                  ),
+                                );
+                              },
+                            ),
+                            RaisedButton(
+                              color: isThemeCurrentlyDark(context) ? MyColors.twitter : Colors.blue[900],
+                              splashColor: Colors.pinkAccent,
+                              child: Text(
+                                "About Page",
+                                style: isThemeCurrentlyDark(context) ? BodyStylesDefault.white : BodyStylesDefault.black,
+                              ),
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  CupertinoPageRoute(
+                                    builder: (context) {
+                                      return MyAboutPage();
+                                    },
+                                  ),
+                                );
+                              },
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ))
+          ],
         ),
       ),
-
+      floatingActionButton: FloatingActionButton(
+        heroTag: 'fab',
+        child: isThemeCurrentlyDark(context)
+            ? Icon(
+          EvaIcons.sun,
+          size: 30.0,
+        ) //show sun icon when in dark mode
+            : Icon(
+          EvaIcons.moon,
+          size: 26.0,
+        ),
+        //show moon icon when in light mode
+        tooltip: isThemeCurrentlyDark(context) ? 'Switch to light mode' : 'Switch to dark mode',
+        foregroundColor: invertInvertColorsStrong(context),
+        backgroundColor: invertInvertColorsTheme(context),
+        elevation: 5.0,
+        onPressed: () {
+          DynamicTheme.of(context)
+              .setBrightness(Theme.of(context).brightness == Brightness.dark ? Brightness.light : Brightness.dark);
+        },
+      ),
     );
   }
 }
