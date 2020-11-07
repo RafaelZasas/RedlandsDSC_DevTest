3072216bcbc280e1e04d5dba5a4cecb8d7384427
Rafael Zasas <zasas.rafi@gmail.com>
2020-11-07 08:58:32 -0800

    Firebase hotFix 🐛🚑
    What was the issue: 🐛
    Attempted to use Firebase Crashlytics without installing correctly.
    
    Steps to solve issue: 📝
    Add this line to android/build.gradle under dependencies :
         classpath 'com.google.firebase:firebase-crashlytics-gradle:2.3.0' 
    
    Under android/app/build.gradle add this under dependencies:
    
        implementation 'com.google.firebase:firebase-crashlytics:17.2.2'
    
    and then apply the plugin using: 
    
    apply plugin: 'com.google.firebase.crashlytics' 
    
    In other news...
    
    ➖ firebase_in_app_messaging: ^0.2.2  from dependencies
    
    📈 Updated analytics to track screen changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:cloud_functions/cloud_functions.dart';
 import 'package:dsc_ur/utils/colors.dart';
 //firebase imports
 import 'package:dynamic_theme/dynamic_theme.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:firebase_analytics/observer.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
@@ -12,15 +14,19 @@ import 'Dashboard_UI_Pages/note_page.dart';
 FirebaseAnalytics analytics;
 
 Future<void> main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+  WidgetsFlutterBinding.ensureInitialized(); // ensure that firebase is initialized before compiling
   // future added for async modifier
-  await Firebase.initializeApp(); // initialize firebase
+  await Firebase.initializeApp(); // NOTE* Must initialize firebase before anything else
   analytics = FirebaseAnalytics(); // initialize analytics
   FirebaseCrashlytics.instance
       .setCrashlyticsCollectionEnabled(true); // enable crash info collection
   FlutterError.onError = (FlutterErrorDetails details) {
     FirebaseCrashlytics.instance.recordFlutterError(details); // record when errors/crashes happen
   };
+
+  FirebaseFunctions functions = FirebaseFunctions.instance; // init firebase cloud functions
+  FirebaseAuth auth = FirebaseAuth.instance; // init firebase authentication
+
   runApp(MyApp());
 }
 
@@ -43,6 +49,10 @@ class MyApp extends StatelessWidget {
           title: 'DSC Redlands',
           theme: theme,
           home: MyNotePage(),
+          navigatorObservers: [
+            // track when user changes screens
+            FirebaseAnalyticsObserver(analytics: analytics),
+          ],
         );
       },
     );
