c49692d275b0183fbbaa3a5c61932bcf2cbb3eb5
Rafael Zasas <zasas.rafi@gmail.com>
2020-11-06 23:47:46 -0800

    Added Firebase 🔥
    Added Firebase Configuration files ⚡️
    
    
    Dependencies:
    
    ➕  firebase_core: ^0.5.2  
    ➕firebase_analytics: ^6.2.0
    ➕firebase_auth: ^0.18.3 
    ➕cloud_firestore: ^0.14.3  i
    ➕cloud_functions: ^0.7.0+1 
    ➕ firebase_crashlytics: ^0.2.3 
    ➕ firebase_performance: ^0.4.2
    ➕ firebase_in_app_messaging: ^0.2.2

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,33 @@
 import 'package:dsc_ur/utils/colors.dart';
+//firebase imports
 import 'package:dynamic_theme/dynamic_theme.dart';
+import 'package:firebase_analytics/firebase_analytics.dart';
+import 'package:firebase_analytics/observer.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 
 import 'Dashboard_UI_Pages/note_page.dart';
 
-void main() => runApp(MyApp());
+FirebaseAnalytics analytics;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // future added for async modifier
+  await Firebase.initializeApp(); // initialize firebase
+  analytics = FirebaseAnalytics(); // initialize analytics
+  FirebaseCrashlytics.instance
+      .setCrashlyticsCollectionEnabled(true); // enable crash info collection
+  FlutterError.onError = (FlutterErrorDetails details) {
+    FirebaseCrashlytics.instance.recordFlutterError(details); // record when errors/crashes happen
+  };
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
+  static FirebaseAnalytics analytics = FirebaseAnalytics();
+  static FirebaseAnalyticsObserver observer = FirebaseAnalyticsObserver(analytics: analytics);
+
   @override
   Widget build(BuildContext context) {
     return DynamicTheme(
@@ -19,7 +40,7 @@ class MyApp extends StatelessWidget {
       ),
       themedWidgetBuilder: (context, theme) {
         return MaterialApp(
-          title: 'Dashboard Reborn',
+          title: 'DSC Redlands',
           theme: theme,
           home: MyNotePage(),
         );
