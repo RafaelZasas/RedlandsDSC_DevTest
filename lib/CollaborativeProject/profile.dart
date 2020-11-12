1a62582b67b97ea52adf8405ce8b0909e8c92ce3
Rafael Zasas <zasas.rafi@gmail.com>
2020-11-11 18:08:39 -0800

    Added Profile Page ✨
    Doing this to show how we can separate the coding into parts

---
+++ lib/CollaborativeProject/profile.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class ProfilePage extends StatefulWidget {
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Text(
+            "Profile Page",
+            style: TextStyle(fontSize: 50),
+          ),
+        ),
+      ),
+    );
+  }
+}
