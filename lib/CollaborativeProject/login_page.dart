e0a96adfe71978c111fd938856ae5f287e8d034c
Rafael Zasas <zasas.rafi@gmail.com>
2020-11-11 18:09:57 -0800

    Added Login Page ✨
    Doing this to show how we can separate the code into parts

---
+++ lib/CollaborativeProject/login_page.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        child: Center(
+          child: Text(
+            "Review App",
+            style: TextStyle(fontSize: 50),
+          ),
+        ),
+      ),
+    );
+  }
+}
